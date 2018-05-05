6af496a9421326ef3ad0b3da8f5fd7fc0452ba52
Russell Briggs <russ@paraflyer.net>
2018-05-05 18:11:35 +1200

    Add tests for production (json) mode

--- src/__tests__/logger.tests.ts
+++ src/__tests__/logger.tests.ts
@@ -37,7 +37,7 @@ describe('logger', () => {
             }
             else {
                 ctx.status = options.statusCode || 200;
-                ctx.body = 'Hello Tests!';
+                ctx.body = 'test_response';
             }
         });
         server = app.listen();
@@ -57,7 +57,7 @@ describe('logger', () => {
         [
             200,
             201,
-            204,
+            202,
             302,
         ]
         .map((statusCode) => {
@@ -66,6 +66,7 @@ describe('logger', () => {
 
                 const response = await request.get('/');
                 expect(response.status).toEqual(statusCode);
+                expect(response.text).toEqual('test_response');
                 expect(logOutput).toHaveLength(1);
 
                 const data = logData[0];
@@ -86,6 +87,7 @@ describe('logger', () => {
 
                 const response = await request.get('/');
                 expect(response.status).toEqual(statusCode);
+                expect(response.text).toEqual('test_response');
                 expect(logOutput).toHaveLength(1);
 
                 const data = logData[0];
@@ -133,6 +135,137 @@ describe('logger', () => {
 
             expect(logOutput[0]).toEqual(expectedLog1);
             expect(logOutput[1]).toEqual(expectedLog2);
+        });
+
+    });
+
+    describe('production (json) mode', () => {
+
+        beforeAll(() => {
+            process.env.NODE_ENV = 'not_development___';
+        });
+
+        [
+            200,
+            201,
+            202,
+            302,
+        ]
+        .map((statusCode) => {
+            it('logs successful request: ' + statusCode, async () => {
+                setup({ statusCode });
+
+                const response = await request.get('/');
+                expect(response.status).toEqual(statusCode);
+                expect(response.text).toEqual('test_response');
+                expect(logOutput).toHaveLength(1);
+
+                const data = logData[0];
+                expect(JSON.parse(logOutput[0])).toEqual(
+                    {
+                        timestamp: data.timestamp,
+                        method: 'GET',
+                        url: '/',
+                        query: {},
+                        host: data.host,
+                        userAgent: data.userAgent,
+                        remoteAddress: data.remoteAddress,
+                        responseTime: data.responseTime,
+                        statusCode,
+                    },
+                );
+            });
+        });
+
+        [
+            400,
+            401,
+            404,
+        ]
+        .map((statusCode) => {
+            it('logs problem request: ' + statusCode, async () => {
+                setup({ statusCode });
+
+                const response = await request.get('/');
+                expect(response.status).toEqual(statusCode);
+                expect(response.text).toEqual('test_response');
+                expect(logOutput).toHaveLength(1);
+
+                const data = logData[0];
+                expect(JSON.parse(logOutput[0])).toEqual(
+                    {
+                        timestamp: data.timestamp,
+                        method: 'GET',
+                        url: '/',
+                        query: {},
+                        host: data.host,
+                        userAgent: data.userAgent,
+                        remoteAddress: data.remoteAddress,
+                        responseTime: data.responseTime,
+                        statusCode,
+                    },
+                );
+            });
+        });
+
+        [
+            { ctxThrow: 'Unauthorised', statusCode: 401 },
+            { ctxThrow: 'Cant find it bruv!', statusCode: 404 },
+        ]
+        .map((options) => {
+            it('handles ctx.throw(): ' + options.statusCode, async () => {
+                setup(options);
+
+                const response = await request.get('/');
+                expect(response.status).toEqual(options.statusCode);
+                expect(response.text).toEqual(options.ctxThrow);
+
+                expect(logOutput).toHaveLength(1);
+
+                const data = logData[0];
+                expect(JSON.parse(logOutput[0])).toEqual(
+                    {
+                        timestamp: data.timestamp,
+                        method: 'GET',
+                        url: '/',
+                        query: {},
+                        host: data.host,
+                        userAgent: data.userAgent,
+                        remoteAddress: data.remoteAddress,
+                        responseTime: data.responseTime,
+                        statusCode: options.statusCode,
+                        errorMessage: data.errorMessage,
+                        errorStack: data.errorStack,
+                    },
+                );
+            });
+        });
+
+        it('handles other thrown errors', async () => {
+            setup({ throw: 'Ack nooo!!!' });
+
+            const response = await request.get('/');
+            expect(response.status).toEqual(500);
+            expect(response.text).toEqual('Internal Server Error');
+
+            expect(logOutput).toHaveLength(1);
+
+            const data = logData[0];
+            expect(JSON.parse(logOutput[0])).toEqual(
+                {
+                    timestamp: data.timestamp,
+                    method: 'GET',
+                    url: '/',
+                    query: {},
+                    host: data.host,
+                    userAgent: data.userAgent,
+                    remoteAddress: data.remoteAddress,
+                    responseTime: data.responseTime,
+                    statusCode: 500,
+                    errorMessage: data.errorMessage,
+                    errorStack: data.errorStack,
+                },
+            );
         });
 
     });
